a1d88b98158da2efed25b87b88753a5fa38e9125
Faisal Khan <faisalkhan1222@gmail.com>
2016-06-13 16:10:53 -0400

    created repo project almost done

---
+++ script.js
@@ -0,0 +1,89 @@
+$("body").css("background-color" , "black");
+var currentSong = 0;
+var isplaying = 'false';
+var audioElement = document.createElement('audio');
+
+var audio_files = [
+  "1.mp3",
+  "2.mp3",
+  "3.mp3",
+  "4.mp3",
+  "5.mp3",
+  "6.mp3",
+  "7.mp3",
+  "8.mp3",
+  "9.mp3"
+]
+
+
+//$('#list').html('<li id="1stSong">'+audio_files[0]+'</li>');
+
+$(document).ready(function(){
+console.log("The document is ready the songs were loaded properly!");
+
+function JukeBox(){
+
+  this.previousTrack = function(){
+    isplaying = 'false';
+    if(currentSong > 0){
+      console.log("isplaying: " + isplaying);
+      currentSong = currentSong - 1;
+      this.playTrack(currentSong);
+    }
+  }
+
+
+  this.playTrack = function (currentSong){
+
+    console.log("isplaying: " + isplaying);
+    audioElement.setAttribute('src', audio_files[currentSong]);
+    if(isplaying == 'false'){
+      isplaying = 'true';
+      audioElement.play();
+      console.log("play pressed");
+
+    }
+    else if(isplaying == 'true'){
+      isplaying = 'false';
+      audioElement.pause();
+      console.log("stop pressed");
+    }
+    }
+
+
+  this.nextTrack = function(){
+    isplaying = 'false';
+    if(currentSong < 10){
+      console.log("isplaying: " + isplaying);
+      currentSong = currentSong + 1;
+      this.playTrack(currentSong);
+    }
+  }
+}
+
+myJukePlayer = new JukeBox();
+
+
+$('#list').append('<li id="1stSong" onclick = "myJukePlayer.playTrack(0)">'+audio_files[0]+'</li>');
+$('#list').append('<li id="2ndSong" onclick = "playTrack(1)">'+audio_files[1]+'</li>');
+$('#list').append('<li id="3rdSong" onclick = "playTrack(2)">'+audio_files[2]+'</li>');
+$('#list').append('<li id="4thSong" onclick = "playTrack(3)">'+audio_files[3]+'</li>');
+$('#list').append('<li id="5thSong" onclick = "playTrack(4)">'+audio_files[4]+'</li>');
+$('#list').append('<li id="6thSong" onclick = "playTrack(5)">'+audio_files[5]+'</li>');
+$('#list').append('<li id="7thSong" onclick = "playTrack(6)">'+audio_files[6]+'</li>');
+$('#list').append('<li id="8thSong" onclick = "playTrack(7)">'+audio_files[7]+'</li>');
+$('#list').append('<li id="9thSong" onclick = "playTrack(8)">'+audio_files[8]+'</li>');
+
+
+$("#playTrack").on("click", function(){
+  myJukePlayer.playTrack(currentSong);
+});
+$("#nextTrack").on("click", function(){
+  myJukePlayer.nextTrack();
+});
+
+$("#previousTrack").on("click", function(){
+  myJukePlayer.previousTrack();
+});
+
+});
