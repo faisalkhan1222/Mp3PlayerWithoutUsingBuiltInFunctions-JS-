bf0955728aa1008ca6d610fd4436e78d4fa2e031
faisalkhan1222 <faisalkhan1222@gmail.com>
2017-01-10 21:45:29 -0500

    added structure to the files

--- javaScript/script.js
+++ javaScript/script.js
@@ -9,15 +9,15 @@ An Mp3 player without using the built in player
 //All the mp3 files are downloaded from the following website(open source)
 //http://www.newgrounds.com/audio
 var audio_files = [
-  "1.mp3",
-  "2.mp3",
-  "3.mp3",
-  "4.mp3",
-  "5.mp3",
-  "6.mp3",
-  "7.mp3",
-  "8.mp3",
-  "9.mp3"
+  "../mp3Files/1.mp3",
+  "../mp3Files/2.mp3",
+  "../mp3Files/3.mp3",
+  "../mp3Files/4.mp3",
+  "../mp3Files/5.mp3",
+  "../mp3Files/6.mp3",
+  "../mp3Files/7.mp3",
+  "../mp3Files/8.mp3",
+  "../mp3Files/9.mp3"
 ]
 //variable to keep track of which mp3 is playing
 var currentSong = 0;
@@ -134,16 +134,21 @@ $(document).ready(function(){
   //new object of type Juke Box
   myJukePlayer = new JukeBox();
 
+  console.log(audio_files[0]);
+
+// In case you want to see the exact file name that you uploaded on the screen
+//  $('#list').append('<li id="2ndSong" onclick = "myJukePlayer.playRequest(1)">'+audio_files[1]+'</li>');
+
   //list all the tracks from the array to the main page and add an event on click so that we can play it when clicked
-  $('#list').append('<li id="1stSong" onclick = "myJukePlayer.playRequest(0)">'+audio_files[0]+'</li>');
-  $('#list').append('<li id="2ndSong" onclick = "myJukePlayer.playRequest(1)">'+audio_files[1]+'</li>');
-  $('#list').append('<li id="3rdSong" onclick = "myJukePlayer.playRequest(2)">'+audio_files[2]+'</li>');
-  $('#list').append('<li id="4thSong" onclick = "myJukePlayer.playRequest(3)">'+audio_files[3]+'</li>');
-  $('#list').append('<li id="5thSong" onclick = "myJukePlayer.playRequest(4)">'+audio_files[4]+'</li>');
-  $('#list').append('<li id="6thSong" onclick = "myJukePlayer.playRequest(5)">'+audio_files[5]+'</li>');
-  $('#list').append('<li id="7thSong" onclick = "myJukePlayer.playRequest(6)">'+audio_files[6]+'</li>');
-  $('#list').append('<li id="8thSong" onclick = "myJukePlayer.playRequest(7)">'+audio_files[7]+'</li>');
-  $('#list').append('<li id="9thSong" onclick = "myJukePlayer.playRequest(8)">'+audio_files[8]+'</li>');
+  $('#list').append('<li id="1stSong" onclick = "myJukePlayer.playRequest(0)"> 1 </li>');
+  $('#list').append('<li id="2ndSong" onclick = "myJukePlayer.playRequest(1)"> 2 </li>');
+  $('#list').append('<li id="3rdSong" onclick = "myJukePlayer.playRequest(2)"> 3 </li>');
+  $('#list').append('<li id="4thSong" onclick = "myJukePlayer.playRequest(3)"> 4 </li>');
+  $('#list').append('<li id="5thSong" onclick = "myJukePlayer.playRequest(4)"> 5 </li>');
+  $('#list').append('<li id="6thSong" onclick = "myJukePlayer.playRequest(5)"> 6 </li>');
+  $('#list').append('<li id="7thSong" onclick = "myJukePlayer.playRequest(6)"> 7 </li>');
+  $('#list').append('<li id="8thSong" onclick = "myJukePlayer.playRequest(7)"> 8 </li>');
+  $('#list').append('<li id="9thSong" onclick = "myJukePlayer.playRequest(8)"> 9 </li>');
 
   //set all the events based  on  the designated buttons(play,pause etc)
   $("#playTrack").on("click", function(){
